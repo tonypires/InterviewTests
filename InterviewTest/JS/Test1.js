3681f9bcf27ecbbc3bbbfb17177b293842a83ceb
Tony Pires <ampires9@gmail.com>
2021-02-25 14:34:41 +0000

    Added a couple JS and SQL tests and organized files into folders.

---
+++ InterviewTest/JS/Test1.js
@@ -0,0 +1,27 @@
+﻿
+/*
+ * Identify why the JS below does not display 'This is my response' in the console when run.
+ * 
+ * You can use node to run this js file to see it's response in your terminal:  node Test1.js
+ * 
+ * */
+
+
+function showResponse(response) {
+    console.log(response);
+}
+
+let myPromise = new Promise(function (myResolve, myReject) {
+    let resp = "This is my response";
+
+    try {
+        return resp;
+    } catch (error) {
+        myReject("Error");
+    }
+});
+
+myPromise.then(
+    function (value) { showResponse(value); },
+    function (error) { showResponse(error); }
+);
---
+++ InterviewTest/SQL/SQLTest2.sql
@@ -0,0 +1,14 @@
+﻿
+-- 1.  Information about city names is kept in two tables:
+
+-- TABLE Europe
+--     Id INTEGER NOT NULL PRIMARY KEY,
+--     name NVARCHAR(50) NOT NULL
+
+-- TABLE Africa
+--     Id INTEGER NOT NULL PRIMARY KEY,
+--     name NVARCHAR(50) NOT NULL
+
+-- Write a query that returns a unique list of all city names
+
+
